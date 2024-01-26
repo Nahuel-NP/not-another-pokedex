a1f221726236b5838b413ecbc85d39d146435239
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-26 11:00:17 -0300

    init Pokemon Card

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full bg-teal-800 max-w-xs rounded-xl p-4 cursor-pointer">
+    <h4 class="text-white text-center font-semibold">
+      #{{pokemon.id}} - {{ pokemon.name }}
+    </h4>
+    <div class="flex gap-2 justify-center">
+      <span v-for="pokemonType in pokemon.types" :key="pokemon.name + pokemonType.type.name">{{ pokemonType.type.name }}</span>
+    </div>
+    <img :src="pokemon.sprites.other?.dream_world.front_default" class="w-full aspect-square" :alt="`pokemon ${pokemon.name}`">
+    <p class="text-center">weight: {{ pokemon.weight }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Pokemon } from '@/interfaces/pokeapi'
+
+defineProps<{
+  pokemon: Pokemon
+}>()
+</script>
