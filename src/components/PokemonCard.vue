b34ba07cb5b85b6593cf564fc6ac3483579223b4
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-26 20:34:34 -0300

    fix card content alignment

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -17,11 +17,11 @@ const showDetails = (pokemon: Pokemon) => {
 
 <template>
   <div
-    class="w-full bg-teal-500 max-w-xs rounded-xl p-4 cursor-pointer hover:scale-105 transition-all hover:bg-teal-400 relative"
+    class="w-full bg-teal-500 flex flex-col justify-between max-w-xs rounded-xl pt-5 pb-4 px-2 cursor-pointer hover:scale-105 transition-all hover:bg-teal-400 relative"
     @click="showDetails(pokemon)"
   >
     <p class="absolute text-xs top-1 right-2 text-white">#{{ pokemon.id }}</p>
-    <h4 class="text-white text-lg capitalize text-center font-bold">{{ pokemon.name }}</h4>
+    <h4 class="text-white text-base  capitalize text-center font-bold">{{ pokemon.name }}</h4>
     <p class="flex gap-2 text-gray-800 justify-center">
       <span v-for="pokemonType in pokemon.types" :key="pokemon.name + pokemonType.type.name">{{
         pokemonType.type.name
@@ -34,6 +34,6 @@ const showDetails = (pokemon: Pokemon) => {
       class="w-full aspect-square my-1"
       :alt="`pokemon ${pokemon.name}`"
     />
-    <p class="text-center">weight: {{ pokemon.weight }} lbs.</p>
+    <p class="text-center ">weight: {{ pokemon.weight }} lbs.</p>
   </div>
 </template>
