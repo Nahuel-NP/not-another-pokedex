4baa3ef7adabab636f4bddcefd45d1dee4b74d59
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-26 14:05:07 -0300

    user Vue-query

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,9 +5,9 @@ import type { PokeResponse, Pokemon } from '@/interfaces/pokeapi'
 import { getPokemonInfo } from '../utils/getPokemon'
 import PokemonCard from '@/components/PokemonCard.vue'
 import LoaderBall from '@/components/shared/LoaderBall.vue'
+import { useQuery } from '@tanstack/vue-query'
 
 const pokemons = ref<Pokemon[]>([])
-const isLoading = ref(true)
 
 const getPokemons = async () => {
   const { data } = await pokeApi.get<PokeResponse>('/pokemon', {
@@ -22,11 +22,17 @@ const getPokemons = async () => {
   })
 
   const [...results] = await Promise.all(promises)
-  isLoading.value = false
+
   pokemons.value = results
+  console.log(pokemons.value)
+  return pokemons
 }
 
-getPokemons()
+const { data, isFetching } = useQuery({
+  queryKey: ['pokemons'],
+  queryFn: getPokemons,
+  staleTime: 60 * 1000
+})
 </script>
 
 <template>
@@ -35,12 +41,12 @@ getPokemons()
       class="fixed top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(teal,#00091d_1px)] bg-[size:20px_20px]"
     ></div>
     <div
-      v-if="!isLoading"
+      v-if="!isFetching"
       class="grid px-4 container grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 w-full place-items-center"
     >
-      <PokemonCard v-for="pokemon in pokemons" :key="pokemon.id" :pokemon="pokemon" />
+      <PokemonCard v-for="pokemon in data" :key="pokemon.id" :pokemon="pokemon" />
     </div>
-    
+
     <LoaderBall v-else />
   </main>
 </template>
