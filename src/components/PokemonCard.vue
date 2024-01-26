98b4b6ba6c3c12d22e8c8c5100b65629771c9660
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-26 13:09:26 -0300

    create transition to details view

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,20 +1,47 @@
-<template>
-  <div class="w-full bg-teal-800 max-w-xs rounded-xl p-4 cursor-pointer">
-    <h4 class="text-white text-center font-semibold">
-      #{{pokemon.id}} - {{ pokemon.name }}
-    </h4>
-    <div class="flex gap-2 justify-center">
-      <span v-for="pokemonType in pokemon.types" :key="pokemon.name + pokemonType.type.name">{{ pokemonType.type.name }}</span>
-    </div>
-    <img :src="pokemon.sprites.other?.dream_world.front_default" class="w-full aspect-square" :alt="`pokemon ${pokemon.name}`">
-    <p class="text-center">weight: {{ pokemon.weight }}</p>
-  </div>
-</template>
-
 <script setup lang="ts">
 import type { Pokemon } from '@/interfaces/pokeapi'
+import { useRouter } from 'vue-router'
+import { usePokemonStore } from '../stores/pokemonStore'
 
 defineProps<{
   pokemon: Pokemon
 }>()
+
+const router = useRouter()
+
+const showDetails = (pokemon: Pokemon) => {
+  const pokemonStore = usePokemonStore()
+  pokemonStore.setActivePokemon(pokemon)
+  // @ts-ignore
+  if (!document.startViewTransition) {
+    router.push(`/pokemon/${pokemon.name}`)
+    return
+  }
+  // @ts-ignore
+  document.startViewTransition(() => {
+    router.push(`/pokemon/${pokemon.name}`)
+  })
+}
 </script>
+
+<template>
+  <div
+    class="w-full bg-teal-600 max-w-xs rounded-xl p-4 cursor-pointer"
+    @click="showDetails(pokemon)"
+  >
+    <h4 class="text-white text-center font-semibold">#{{ pokemon.id }} - {{ pokemon.name }}</h4>
+    <div class="flex gap-2 justify-center"
+    :style="{ viewTransitionName: pokemon.name }">
+      <span v-for="pokemonType in pokemon.types" :key="pokemon.name + pokemonType.type.name">{{
+        pokemonType.type.name
+      }}</span>
+    </div>
+    <img
+      :src="pokemon.sprites.other?.dream_world.front_default"
+      class="w-full aspect-square"
+      :alt="`pokemon ${pokemon.name}`"
+      
+    />
+    <p class="text-center">weight: {{ pokemon.weight }}</p>
+  </div>
+</template>
--- src/views/PokemonDetails.vue
+++ src/views/PokemonDetails.vue
@@ -1,13 +1,62 @@
-<template>
-  <div>
-    Hola
-  </div>
-</template>
-
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { usePokemonStore } from '../stores/pokemonStore'
+import { getPokemonInfo } from '../utils/getPokemon'
+import LoaderBall from '@/components/shared/LoaderBall.vue'
 
+const store = usePokemonStore()
+const route = useRoute()
+const isLoading = ref(false)
+
+
+const getPokemon = async (name: string) => {
+  isLoading.value = true
+  const pokemon = await getPokemonInfo(name)
+
+  if (pokemon) {
+    store.setActivePokemon(pokemon)
+  }
+  isLoading.value = false
+}
+
+if (!store.getActivePokemon) {
+  const { name } = route.params
+  getPokemon(name as string)
+}
+
+const router = useRouter()
+
+const backToList = () => {
+  // @ts-ignore
+  if (!document.startViewTransition) {
+    router.push('/')
+    return
+  }
+  // @ts-ignore
+  document.startViewTransition(() => {
+    router.push('/')
+  })
+}
 </script>
 
-<style scoped>
-
-</style>
+<template>
+  <main class="min-h-screen flex flex-col justify-center items-center">
+    <button @click="backToList" class="text-white">Back</button>
+    <div
+      class="fixed top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(teal,#00091d_1px)] bg-[size:20px_20px]"
+    ></div>
+    <div
+      v-if="store.getActivePokemon"
+      class="w-full flex max-w-md p-12"
+      :style="{ viewTransitionName: store.getActivePokemon.name }"
+    >
+      <img
+        class="w-full"
+        :src="store.getActivePokemon.sprites.other?.dream_world.front_default"
+        :alt="store.getActivePokemon.name"
+      />
+    </div>
+    <LoaderBall v-if="isLoading" />
+  </main>
+</template>
