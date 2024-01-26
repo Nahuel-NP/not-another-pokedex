2e9da134fd427c4d79a51b6ddd17add2464c16d7
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-26 13:08:40 -0300

    create LoaderBall component

---
+++ src/components/shared/LoaderBall.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p class="text-xl text-white text-center font-bold mb-10">Loading...</p>
+    <img src="/pokeball.svg" alt="pokeball loader" class="max-w-32 animate-spin" />
+  </div>
+</template>
