9cdbdf5cc3ed975121715b4f73dfc11feb9bdb0d
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-27 13:23:20 -0300

    change text

--- src/components/shared/LoaderBall.vue
+++ src/components/shared/LoaderBall.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center items-center">
-    <p class="text-xl text-white text-center font-bold mb-10">Loading...</p>
+    <p class="text-xl text-white text-center font-bold mb-10">Cargando...</p>
     <img src="/pokeball.svg" alt="pokeball loader" class="max-w-32 animate-spin" />
   </div>
 </template>
