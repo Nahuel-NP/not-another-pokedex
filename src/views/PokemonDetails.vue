fdbb05fe19e9552acb094620e55ddf5e05a654ff
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-26 22:18:00 -0300

    fix image

--- src/views/PokemonDetails.vue
+++ src/views/PokemonDetails.vue
@@ -34,14 +34,21 @@ const { handleTransition } = useTransition()
       class="fixed top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(teal,#00091d_1px)] bg-[size:20px_20px]"
     ></div>
     <h2 class="text-teal-500 text-4xl font-bold capitalize">{{ store.getActivePokemon?.name }}</h2>
-    <div v-if="store.getActivePokemon" class="w-full grid gap-8 items-center lg:grid-cols-2 max-w-5xl p-4 md:p-10">
+    <div
+      v-if="store.getActivePokemon"
+      class="w-full grid gap-8 items-center lg:grid-cols-2 max-w-5xl p-4 md:p-10"
+    >
       <img
         class="w-full max-w-sm mx-auto aspect-square"
         :style="{ viewTransitionName: store.getActivePokemon.name }"
-        :src="store.getActivePokemon.sprites.other?.dream_world.front_default"
+        :src="
+          store.getActivePokemon.sprites.other?.dream_world.front_default !== null
+            ? store.getActivePokemon.sprites.other?.dream_world.front_default
+            : store.getActivePokemon.sprites.other?.['official-artwork'].front_default
+        "
         :alt="store.getActivePokemon.name"
       />
-      <div class="bg-gray-200  border-gray-700 border-4 p-4 rounded-md text-lg">
+      <div class="bg-gray-200 border-gray-700 border-4 p-4 rounded-md text-lg">
         <p class="flex gap-2">
           Tipo:
           <span v-for="PokemonType in store.getActivePokemon.types" :key="PokemonType.type.name">
