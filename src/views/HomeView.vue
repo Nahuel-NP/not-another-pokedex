be8289375c4004e7039da9cc4327d959c746583a
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-27 12:58:11 -0300

    search by name and code refactor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,57 +1,43 @@
 <script lang="ts" setup>
 import { ref, type Ref } from 'vue'
-import pokeApi from '@/api/pokeApi'
-import type { PokeResponse } from '@/interfaces/pokeapi'
 import { getPokemonInfo } from '../utils/getPokemonByName'
 import PokemonCard from '@/components/PokemonCard.vue'
 import LoaderBall from '@/components/shared/LoaderBall.vue'
-import SearchIcon from '@/components/icons/SearchIcon.vue'
+import SearchForm from '@/components/home/SearchForm.vue'
 import NextPageIcon from '@/components/icons/NextPagIcon.vue'
 import PrevPageIcon from '@/components/icons/PrevPagIcon.vue'
 import { useQuery, keepPreviousData } from '@tanstack/vue-query'
+import { getPokemons } from '../utils/getAllPokemons'
 
-const nameToSearch = ref('')
-const getPokemons = async (page: Ref<number>, limit: number = 24) => {
-  const { data } = await pokeApi.get<PokeResponse>('/pokemon', {
-    params: {
-      limit,
-      offset: (page.value - 1) * limit
-    }
-  })
-  console.log({ data })
-  const promises = data.results.map(async (pokemon) => {
-    const pokemonResponse = await getPokemonInfo(pokemon.name)
-    return pokemonResponse
-  })
-
-  const [...results] = await Promise.all(promises)
-
-  const totalPages = Math.ceil(data.count / limit)
-
-  return {
-    ...data,
-    totalPages,
-    results
-  }
-}
-
-const searchByName = async (event: any) => {
-  event.preventDefault()
+const searchByName = async (nameToSearch: Ref<string>) => {
   const data = await getPokemonInfo(nameToSearch.value.toLowerCase())
-  /*   if (data) {
-    pokemons.value = [data]
-    return
-  }
-  pokemons.value = [] */
+  console.log(data)
+  return data
 }
 
 const page = ref(1)
 
-const { data, isFetching, isPlaceholderData } = useQuery({
+const {
+  data: allPokemons,
+  isFetching,
+  isPlaceholderData
+} = useQuery({
   queryKey: ['pokemons', page],
   queryFn: () => getPokemons(page),
   staleTime: 60 * 1000 * 10, //10 minutos
   placeholderData: keepPreviousData
+})
+
+const nameToSearch = ref('')
+const isFiltering = ref(false)
+const tempName = ref('')
+
+const { data: pokemonByName } = useQuery({
+  queryFn: () => searchByName(nameToSearch),
+  queryKey: ['pokemon', nameToSearch],
+  enabled: isFiltering,
+  staleTime: 60 * 1000 * 10,
+  retry: false
 })
 
 const prevPage = () => {
@@ -63,6 +49,24 @@ const nextPage = () => {
     page.value = page.value + 1
   }
 }
+
+//todo: change any type
+const handleFilter = (event: any) => {
+  event.preventDefault()
+  if (tempName.value !== '') {
+    isFiltering.value = true
+    nameToSearch.value = tempName.value
+  }
+}
+
+const handlerResetFilter = () => {
+  isFiltering.value = false
+  nameToSearch.value = ''
+}
+
+const onUpdateInput = (value:string) =>{
+  tempName.value = value
+}
 </script>
 
 <template>
@@ -71,36 +75,28 @@ const nextPage = () => {
       class="fixed top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(teal,#00091d_1px)] bg-[size:20px_20px]"
     ></div>
 
-    <form class="max-w-lg mx-auto mb-8 px-4" @submit="searchByName">
-      <div class="relative">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <SearchIcon />
-        </div>
-        <input
-          type="search"
-          id="default-search"
-          class="block w-full p-4 ps-10 text-sm text-white border border-gray-300 rounded-lg bg-gray-700 focus:ring-teal-500 focus:border-teal-500"
-          placeholder="Search Pokemon"
-          required
-          v-model="nameToSearch"
-        />
-        <button
-          type="submit"
-          class="text-white absolute end-2.5 bottom-2.5 bg-teal-500 hover:bg-teal-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2"
-        >
-          Buscar
-        </button>
-      </div>
-    </form>
+    <SearchForm
+      :handle-filter="handleFilter"
+      :handle-reset-filter="handlerResetFilter"
+      :is-filtering="isFiltering"
+      :temp-name="tempName"
+      @update-input="onUpdateInput"
+    />
 
+    <div v-if="pokemonByName" class="w-full">
+      <PokemonCard :pokemon="pokemonByName" class="mx-auto" />
+    </div>
     <div
       v-if="!isFetching"
       class="grid mx-auto px-4 container grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 w-full place-items-center"
     >
-      <PokemonCard v-for="pokemon in data?.results" :key="pokemon.id" :pokemon="pokemon" />
+      <PokemonCard v-for="pokemon in allPokemons?.results" :key="pokemon.id" :pokemon="pokemon" />
     </div>
 
     <LoaderBall v-else />
+    <!-- <div class="grid place-items-center">
+      <PokemonCard v-if="pokemonByName" :pokemon="pokemonByName" />
+    </div> -->
 
     <nav aria-label="Page navigation" class="flex justify-center mt-8">
       <div class="flex items-center -space-x-px h-10 text-base">
