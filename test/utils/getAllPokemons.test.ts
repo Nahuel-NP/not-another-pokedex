0f97890d44cb281c95d45aa60b51c40521882046
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-01-29 18:36:58 -0300

    Test getAllPokemons

--- test/utils/getAllPokemons.test.ts
+++ test/utils/getAllPokemons.test.ts
@@ -1,7 +1,14 @@
-describe('Test getAllPokemons util', () => { 
-  
-  test('should get initial values', () => { 
-    
-    
-   })
- })
+import { ref } from 'vue';
+import { getPokemons } from '../../src/utils/getAllPokemons';
+describe('Test getAllPokemons util', () => {
+
+  test('should get first 10 pokemons', async() => {
+    const limit = 10
+    const page = ref(1)
+    const {count,results } = await getPokemons(page, limit)
+
+    expect(count).toBeGreaterThan(0)
+    expect(results.length).toBe(limit)
+
+  })
+})
