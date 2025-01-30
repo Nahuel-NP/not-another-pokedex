27d4a6f3de0327cef8e811df81326a0a27646b70
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-01-30 16:30:24 -0300

    test PokemonVIew

--- test/views/HomeView.test.ts
+++ test/views/HomeView.test.ts
@@ -1,7 +1,7 @@
 import router from "@/router"
 import HomeView from "@/views/HomeView.vue"
 import { useQuery, VueQueryPlugin } from "@tanstack/vue-query"
-import { flushPromises, mount } from "@vue/test-utils"
+import { DOMWrapper, flushPromises, mount } from "@vue/test-utils"
 import { createPinia, setActivePinia } from "pinia"
 import { createApp, ref } from "vue"
 
@@ -45,11 +45,33 @@ describe('Test Home View', () => {
 
     await flushPromises();
     // await useQuery finish
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    await new Promise(resolve => setTimeout(resolve, 3000));
     wrapper.vm.$nextTick()
 
     expect(wrapper.html()).not.toContain('Cargando...')
     expect(wrapper.find('[data-testid="grid-elements"]').exists()).toBe(true)
 
   })
+
+  test('should show one pokemon when search by name', async () => {
+
+    const wrapper = mount(HomeView, {
+      global: {
+        plugins: [router, VueQueryPlugin]
+      }
+    })
+
+    const input = wrapper.find('#simple-search') as DOMWrapper<HTMLInputElement>
+    input.element.value = 'pikachu';
+    await input.trigger('input')
+
+    await wrapper.find('form').trigger('submit')
+
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+    expect(wrapper.html()).not.toContain('Cargando...')
+    expect(wrapper.find('h4').text()).toBe('pikachu')
+
+  })
+
 })
